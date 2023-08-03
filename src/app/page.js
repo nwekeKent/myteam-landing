2798bb42828dca1685193726abc65f2ee2b22cab
kent <nwekep14@gmail.com>
2023-08-03 03:05:40 +0100

    - fix left side hero deco for bigger screens

--- src/app/page.js
+++ src/app/page.js
@@ -4,19 +4,15 @@ import Features from "@/components/index/Features";
 import Hero from "@/components/index/Hero";
 import Started from "@/components/index/Started";
 import Testimonial from "@/components/index/Testimonial";
-import Footer from "@/components/layouts/Footer";
-import Navbar from "@/components/layouts/navbar/Navbar";
 import React from "react";
 
 export default function Home() {
 	return (
 		<React.Fragment>
-			<Navbar />
 			<Hero />
 			<Features />
 			<Testimonial />
 			<Started />
-			<Footer />
 		</React.Fragment>
 	);
 }
--- src/components/index/Hero.jsx
+++ src/components/index/Hero.jsx
@@ -5,25 +5,26 @@ import SVG from "react-inlinesvg";
 
 const Hero = () => {
 	return (
-		<section className="section-container hero">
-			<div className="hero__left">
-				<h1 className="hero-heading">Find the</h1>
-				<h1 className="hero-heading">
-					best <span>talent</span>
-				</h1>
-			</div>
+		<section className=" hero">
+			<div className="section-container hero-container">
+				<div className="hero__left">
+					<h1 className="hero-heading">Find the</h1>
+					<h1 className="hero-heading">
+						best <span>talent</span>
+					</h1>
+				</div>
 
-			<div className="hero__right">
-				<div className="text--deco deco--line"></div>
-				<div className="hero__content">
-					<p className="hero-paragraph">
-						Finding the right people and building high performing teams can be
-						hard. Most companies aren’t tapping into the abundance of global
-						talent. We’re about to change that.
-					</p>
+				<div className="hero__right">
+					<div className="text--deco deco--line"></div>
+					<div className="hero__content">
+						<p className="hero-paragraph">
+							Finding the right people and building high performing teams can be
+							hard. Most companies aren’t tapping into the abundance of global
+							talent. We’re about to change that.
+						</p>
+					</div>
 				</div>
 			</div>
-
 			<div className="hero_deco_bottom">
 				<SVG src="/assets/bg-pattern-home-2.svg" />
 			</div>
